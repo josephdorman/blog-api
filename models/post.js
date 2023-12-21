834dc8a87813cd1db217c0f58fd707ae8162b7c4
Joseph Dorman <JDorman555@yahoo.com>
2023-12-20 19:45:49 -0600

    feat: add schema for posts

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+  date: { type: Date, default: Date.now },
+  content: { type: String, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment", required: true }],
+});
+
+modules.exports = mongoose.model("Post", PostSchema);
