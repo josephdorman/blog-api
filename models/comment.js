81a24b09f5c77e5f4e6660dad4c54edbc5215f94
Joseph Dorman <JDorman555@yahoo.com>
2023-12-20 23:01:13 -0600

    fix: typo

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,7 @@ const CommentSchema = new Schema({
   user: { type: String, required: true },
   date: { type: Date, default: Date.now },
   comment: { type: String, required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Post" },
+  postId: { type: Schema.Types.ObjectId, ref: "Post" },
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
