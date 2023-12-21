7b85883e56af3c726a8d729bad1741691f56a864
Joseph Dorman <JDorman555@yahoo.com>
2023-12-20 20:31:03 -0600

    feat: require controller files in index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// Require controller modules.
+const authorController = require("../controllers/authorController");
+const postController = require("../controllers/postController");
+const commentController = require("../controllers/commentController");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.json("Hello World!");
