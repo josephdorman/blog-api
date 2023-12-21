1830dfbfec42ba86ff212014352e8fec73a4760b
Joseph Dorman <JDorman555@yahoo.com>
2023-12-20 19:46:55 -0600

    fix: wrong ref

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+  blog: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   user: { type: string, required: true },
   date: { type: Date, default: Date.now },
   comment: { type: string, required: true },
