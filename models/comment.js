157c46ae4762b6a0e269e58830a918ad6260aebc
Joseph Dorman <JDorman555@yahoo.com>
2023-12-20 21:16:06 -0600

    feat: remove requirement of post

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  blog: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   user: { type: String, required: true },
   date: { type: Date, default: Date.now },
   comment: { type: String, required: true },
